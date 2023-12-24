4edcae45428da2e57917bb62a48684920839c754
mogenedy <mogenedy2030@gmail.com>
2023-12-24 13:57:19 +0200

    layouts

--- routes/backend.php
+++ routes/backend.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\dashboard\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,3 +13,4 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/dashboard/admin',[DashboardController::class,'index']);
